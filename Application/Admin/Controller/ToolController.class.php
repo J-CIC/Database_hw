53516d37e918cb84350f0b8d48cd929964cda89b
CIC <791863347@qq.com>
2017-11-19 13:49:39 +0800

    update Tools

--- Application/Admin/Controller/ToolController.class.php
+++ Application/Admin/Controller/ToolController.class.php
@@ -20,7 +20,7 @@ class ToolController extends Controller
         $objActSheet = $objPHPExcel->getActiveSheet();
 
         if(!is_array($header)||!is_array($content)){
-            throw new /Exception("Wrong Parameters!", 1);
+            throw new \Exception("Wrong Parameters!", 1);
         }
 
         foreach ($header as $key => $value) {
