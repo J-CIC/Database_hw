48be892b0f0905c0d51635da7cb3ac6b40deaad1
CIC <791863347@qq.com>
2017-11-19 18:10:19 +0800

    fail try to namespace of PhpOffice

--- Application/Admin/Controller/ToolController.class.php
+++ Application/Admin/Controller/ToolController.class.php
@@ -1,9 +1,65 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
+use PhpOffice\PhpSpreadsheet;
 
+// spl_autoload_register(function ($class) {
+//     // include 'classes/' . $class . '.class.php';
+//     $file = LIB_PATH."Org/Util/".$class.".php";
+//     // echo $file;
+//     if(file_exists($file))
+//         require_once($file);  
+// },false); 
+// echo LIB_PATH;
 class ToolController extends Controller
 {
+
+
+    // /*
+    // *@param filename string 文件名
+    // *@param header 二维数组，表头属性，一维存放所有表头的数组，第二维存放名字、宽度、key等属性
+    // *@param content 二维数组，数据维用key=>value模式，与表头保持一致
+    // */
+    // public function export2Excel($filename,$header,$content)
+    // {
+    //     set_time_limit(0);
+    //     $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+    //     $objWriter = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
+    //     $objActSheet = $spreadsheet->getActiveSheet();
+
+
+    //     if(!is_array($header)||!is_array($content)){
+    //         throw new \Exception("Wrong Parameters!", 1);
+    //     }
+
+    //     foreach ($header as $key => $value) {
+    //         $alpha = strtoupper(chr($key+65));//大写字母
+    //         $tableHeader = $alpha."1";
+    //         $objActSheet->setCellValue($tableHeader, $value["name"]);
+    //         $spreadsheet->getActiveSheet()->getColumnDimension($alpha)->setWidth($value["width"]);
+    //         $spreadsheet->getActiveSheet()->getStyle($alpha)->getAlignment()->setWrapText(true); 
+    //     }
+    //     $spreadsheet->getDefaultStyle()->getAlignment()->setHorizontal(PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+    //     $spreadsheet->getActiveSheet()->setAutoFilter($spreadsheet->getActiveSheet()->calculateWorksheetDimension());
+        
+
+    //     $savename = date("ymdHi").$filename;
+    //     header("Content-Type: application/vnd.ms-excel;charset=gbk");  
+    //     header("Content-Disposition: attachment; filename=".$savename.".xlsx");  
+    //     header("Pragma: no-cache");
+    //     foreach($content as $i=>$row_content){
+    //         $row = $i+2;
+    //         foreach ($header as $key => $value) {
+    //             $alpha = strtoupper(chr($key+65));//大写字母
+    //             $tableHeader = $alpha.$row;
+    //             $objActSheet->setCellValue($tableHeader, $row_content[$value["key"]]);
+    //         }
+    //     }
+    //     $objWriter = PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+    //     $objWriter->save('php://output'); //文件通过浏览器下载
+    //     die();
+    // }
+
     /*
     *@param filename string 文件名
     *@param header 二维数组，表头属性，一维存放所有表头的数组，第二维存放名字、宽度、key等属性
@@ -54,4 +110,5 @@ class ToolController extends Controller
         $objWriter->save('php://output'); //文件通过浏览器下载
         die();
     }
+
 }
